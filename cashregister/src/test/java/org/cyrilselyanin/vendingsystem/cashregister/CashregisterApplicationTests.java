31724001b5f0d109082cd203a99d6d2a100d2f26
larsnoches <cyr.selyanin@mail.ru>
2022-11-22 22:13:21 +0500

    submodules added

---
+++ cashregister/src/test/java/org/cyrilselyanin/vendingsystem/cashregister/CashregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cyrilselyanin.vendingsystem.cashregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
