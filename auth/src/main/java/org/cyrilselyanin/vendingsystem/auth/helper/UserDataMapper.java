28584594be54633af9be130dc04999e5cf15cca4
larsnoches <cyr.selyanin@mail.ru>
2022-12-10 00:32:30 +0500

    feature/auth, added: toGetUserDto, toGetOrUpdateProfileDto, fromGetOrUpdateProfileDto

--- auth/src/main/java/org/cyrilselyanin/vendingsystem/auth/helper/UserDataMapper.java
+++ auth/src/main/java/org/cyrilselyanin/vendingsystem/auth/helper/UserDataMapper.java
@@ -1,6 +1,7 @@
 package org.cyrilselyanin.vendingsystem.auth.helper;
 
 import org.cyrilselyanin.vendingsystem.auth.domain.Profile;
+import org.cyrilselyanin.vendingsystem.auth.dto.GetOrUpdateProfileDto;
 import org.cyrilselyanin.vendingsystem.auth.dto.GetUserDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -14,11 +15,19 @@ public class UserDataMapper {
 		modelMapper = new ModelMapper();
 	}
 
-	public GetUserDto toDto(Profile profile) {
+	public GetUserDto toGetUserDto(Profile profile) {
 		return modelMapper.map(profile, GetUserDto.class);
 	}
 
-	public Profile fromDto(GetUserDto dto) {
+	public GetOrUpdateProfileDto toGetOrUpdateProfileDto(Profile profile) {
+		return modelMapper.map(profile, GetOrUpdateProfileDto.class);
+	}
+
+	public Profile fromGetUserDto(GetUserDto dto) {
+		return modelMapper.map(dto, Profile.class);
+	}
+
+	public Profile fromGetOrUpdateProfileDto(GetOrUpdateProfileDto dto) {
 		return modelMapper.map(dto, Profile.class);
 	}
 
