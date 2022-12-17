689e985e36440149da975dd58cfefed18ee27125
larsnoches <cyr.selyanin@mail.ru>
2022-12-18 00:43:16 +0500

    feature/auth, added: dto for create user request

---
+++ regularbus/src/main/java/org/cyrilselyanin/vendingsystem/regularbus/dto/auth/CreateUserRequestDto.java
@@ -0,0 +1,22 @@
+package org.cyrilselyanin.vendingsystem.regularbus.dto.auth;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CreateUserRequestDto extends UpdateUserRequestDto {
+
+	@NotBlank(message = "Пароль не может быть пустым.")
+	@Size(
+			min = 4,
+			max = 68,
+			message = "Пароль должен быть от 4 до 68 символов.")
+	private String password;
+
+}
