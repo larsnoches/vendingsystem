2166ec7d82ad8ca7533eb30dee5a495254ab84a5
larsnoches <cyr.selyanin@mail.ru>
2022-12-18 00:43:48 +0500

    feature/auth, changed: dto for update user request

--- regularbus/src/main/java/org/cyrilselyanin/vendingsystem/regularbus/dto/auth/UpdateUserRequestDto.java
+++ regularbus/src/main/java/org/cyrilselyanin/vendingsystem/regularbus/dto/auth/UpdateUserRequestDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -12,7 +11,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @Getter
 @Setter
-public class CuUserRequestDto {
+public class UpdateUserRequestDto {
 
 	@NotBlank(message = "Адрес электронной почты не может быть пустым.")
 	@Size(
@@ -20,16 +19,7 @@ public class CuUserRequestDto {
 			max = 255,
 			message = "Адрес электронной почты должен быть от 6 до 255 символов.")
 	@Email(message = "Такой адрес электронной почты недопустим.")
-	@Column(nullable = false)
 	private String email;
-
-	@NotBlank(message = "Пароль не может быть пустым.")
-	@Size(
-			min = 4,
-			max = 68,
-			message = "Пароль должен быть от 4 до 68 символов.")
-	@Column(nullable = false)
-	private String password;
 
 	@Size(
 			max = 255,
